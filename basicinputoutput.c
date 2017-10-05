fc1ad2c1fa4f9de411bb016780eb347002aadb5d
maxjone <starjonemax@gmail.com>
2017-10-05 22:52:29 +0530

    Add files via upload

---
+++ basicinputoutput.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,d;
+    char str[100];
+    scanf("%d",&d);
+    scanf("%s",str);
+    {
+        m=d*2;
+    }
+    printf("%d\n",m);
+    printf("%s",str);
+    return 0;
+}
